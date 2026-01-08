aee305d58bf208e704ac4b144cde2b06d6899a2e
SaqibMalik822 <saqibmalik5374@gmail.com>
2026-01-08 08:34:38 +0530

    second commit

--- Madlibs_Generator/main.py
+++ Madlibs_Generator/main.py
@@ -3,27 +3,25 @@
 
 def your_story() -> str:
     story:str = input("choose your story, bunny or snakeandman: ").lower().strip()
-    try:
-        story == "bunny" or "snakeandman"
-    except ValueError:
-        print("no such story exists")
-        return
+    if(story not in ("bunny","snakeandman")):
+        return f"{story} no such story exists"
     if story == "bunny":
         bunny_name:str = input("enter the name for bunny: ")
         with open("stories/bunnystory.txt", "r") as f:
             content = f.read()
-        word = "[BUNNY_NAME]"
-        content.replace(word, bunny_name)
-        f.close()
+            word = "[BUNNY_NAME]"
+            content = content.replace(word, bunny_name)
+            f.close()
         return content
     else:
         man_name:str = input("enter the name for the man: ")
         snake_name:str = input("enter the name for the snake: ")
         with open("./stories/snakemanstory.txt", "r") as f:
             content = f.read()
-        word = "[MAN_NAME]"
-        content.replace(word, man_name)
-        content.replace(word, snake_name)
-        f.close()
+            word = "[MAN_NAME]"
+            word2 = "[SNAKE_NAME]"
+            content = content.replace(word, man_name)
+            content = content.replace(word2, snake_name)
+            f.close()
         return content
 print(your_story())
